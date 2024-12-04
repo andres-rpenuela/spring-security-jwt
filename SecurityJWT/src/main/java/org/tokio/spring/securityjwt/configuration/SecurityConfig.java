ecada610fd37118cfe32faf93c35133ba02fee2b
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-04 12:07:47 +0100

    [init] create project empty basic with h2 and security

--- SecurityJWT/src/main/java/org/tokio/spring/securityjwt/configuration/SecurityConfig.java
+++ SecurityJWT/src/main/java/org/tokio/spring/securityjwt/configuration/SecurityConfig.java
@@ -3,8 +3,10 @@ package org.tokio.spring.securityjwt.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -15,13 +17,13 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // Desactiva CSRF para simplificar el acceso a la consola
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**","/actuator/**").permitAll() // Permite acceso a la consola de H2 sin autenticación
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll() // Permite acceso a la consola de H2 sin autenticación
                         .anyRequest().authenticated() // Protege otras rutas
                 )
                 .headers(headers -> headers
                         .frameOptions(frameOptions -> frameOptions.disable()) // Permite el uso de frames para H2
                 );
-
+        http.formLogin(Customizer.withDefaults());
         return http.build();
     }
 }
